caca230d37685af16bb7736645481ce22e5d91d6
YONGMIN <dragonmin25@gmail.com>
2023-05-08 21:45:21 +0900

    local storage에 저장 기능 추가

--- index.js
+++ index.js
@@ -1,47 +1,70 @@
-const form = document.querySelector("form"); // HTML 문서 내의 form 태그를 가져옴
-const input = document.querySelector("input"); // HTML 문서 내의 input 태그를 가져옴
-const todos = document.querySelector(".todos"); // HTML 문서 내의 클래스가 todos인 요소를 가져옴
-const clearBtn = document.querySelector(".clear-btn"); // HTML 문서 내의 클래스가 clear-btn인 요소를 가져옴
+//const form, input, todos, clearBtn 변수는 각각 HTML의 <form>, <input>, <ul>, <button> 요소를 참조.
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const todos = document.querySelector(".todos");
+const clearBtn = document.querySelector(".clear-btn");
 
-const addTodo = (e) => { // 할 일 추가 함수
+//loadTodos()는 localStorage에 저장된 할일 목록을 불러와서 <ul> 요소에 추가
+const loadTodos = () => {
+  const savedTodos = localStorage.getItem("todos");
+  if (savedTodos) {
+    todos.innerHTML = savedTodos;
+  }
+};
+//addTodo()는 새로운 할일을 추가함 <li> 요소를 생성하고, 그 안에 할일 내용과 완료, 삭제 버튼을 포함함.
+const addTodo = (e) => {
   e.preventDefault(); // 이벤트 추가 시 새로고침 X
   const todoText = input.value.trim(); // 입력값에서 양쪽 공백을 제거한 문자열을 가져옴
-  if (todoText === "") { // 문자열이 빈 문자열인 경우, 함수를 종료함
+  if (todoText === "") {
+    // 문자열이 빈 문자열인 경우, 함수를 종료함
     return;
   }
-  const todoItem = document.createElement("li"); // li 요소를 생성함
+  const todoItem = document.createElement("li");
   todoItem.innerHTML = `
     <span class="todo-text">${todoText}</span>
     <div class="btn-container">
       <button class="complete-btn">완료</button>
       <button class="delete-btn">삭제</button>
     </div>
-  `; // li 요소 내에 span과 button 요소를 생성함
-  todos.appendChild(todoItem); // li 요소를 todos 리스트에 추가함
-  input.value = ""; // 입력값을 초기화함
+  `;
+  //생성된 요소를 <ul> 요소에 추가하고, <input> 요소의 값을 초기화.
+  todos.appendChild(todoItem);
+  input.value = "";
+  //현재 할일 목록을 localStorage에 저장.
+  const savedTodos = todos.innerHTML;
+  localStorage.setItem("todos", savedTodos);
 };
-
-const deleteTodo = (e) => { // 할 일 삭제 함수
-  const todoItem = e.target.closest("li"); // 클릭한 버튼에 가장 가까운 li 요소를 찾아옴
-  todos.removeChild(todoItem); // todos 리스트에서 해당 li 요소를 삭제함
+//deleteTodo()는 선택한 할일을 삭제, 삭제 버튼을 클릭하면, 클릭된 버튼의 가장 가까운 <li> 요소를 찾아서 삭제.
+const deleteTodo = (e) => {
+  const todoItem = e.target.closest("li");
+  todos.removeChild(todoItem);
+  //현재 할일 목록을 localStorage에 저장.
+  const savedTodos = todos.innerHTML;
+  localStorage.setItem("todos", savedTodos);
 };
-
-const clearAll = () => { // 할 일 전체 삭제 함수
-  todos.innerHTML = ""; // todos 리스트 내의 모든 요소를 삭제함
+//clearAll()은 모든 할일을 삭제, todos 요소의 innerHTML 속성을 빈 문자열로 설정.
+const clearAll = () => {
+  todos.innerHTML = "";
+  localStorage.removeItem("todos"); //, localStorage에서 할일 목록을 제거함.
 };
-
-const completeTodo = (e) => { // 할 일 완료 함수
-  const todoItem = e.target.closest("li"); // 클릭한 버튼에 가장 가까운 li 요소를 찾아옴
-  todoItem.classList.toggle("completed"); // 해당 li 요소에 completed 클래스를 추가하거나 삭제함
+//completeTodo()는 선택한 할일을 완료합니다. 완료 버튼을 클릭하면, 클릭된 버튼의 가장 가까운 <li> 요소에 completed 클래스를 추가함.
+const completeTodo = (e) => {
+  const todoItem = e.target.closest("li");
+  todoItem.classList.toggle("completed");
+  //현재 할일 목록을 localStorage에 저장.
+  const savedTodos = todos.innerHTML;
+  localStorage.setItem("todos", savedTodos);
 };
-
-form.addEventListener("submit", addTodo); // form 태그에서 submit 이벤트가 발생하면 addTodo 함수를 실행함
-todos.addEventListener("click", (e) => { // todos 리스트에서 click 이벤트가 발생하면 함수를 실행함
-  if (e.target.classList.contains("delete-btn")) { // 클릭한 요소의 클래스가 delete-btn인 경우, deleteTodo 함수를 실행함
+//EventListener들은 각각 할일을 추가하는 폼 입력, 할일 삭제, 완료, 전체 삭제 버튼 클릭 이벤트를 처리.
+form.addEventListener("submit", addTodo);
+todos.addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete-btn")) {
     deleteTodo(e);
   }
-  if (e.target.classList.contains("complete-btn")) { // 클릭한 요소의 클래스가 complete-btn인 경우, completeTodo 함수를 실행함
+  if (e.target.classList.contains("complete-btn")) {
     completeTodo(e);
   }
 });
-clearBtn.addEventListener("click", clearAll); // clearBtn 요소에서 click 이벤트가 발생하면 clearAll 함수를 실행함
+clearBtn.addEventListener("click", clearAll);
+//loadTodos()를 호출해서 이전에 저장된 할일 목록이 있다면 불러옴.
+loadTodos();
